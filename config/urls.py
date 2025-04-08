02a4905c1b13e127be98cdbfcc7bf2e1ab025843
kangyeon <rkddus0517@naver.com>
2025-04-08 16:56:07 +0900

    ➕ django-debug-toolbar 적용

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+
 
 # Swagger 문서 스키마 뷰 설정
 schema_view = get_schema_view(
@@ -34,3 +36,8 @@ urlpatterns = [
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
